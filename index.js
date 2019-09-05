8afb52c2e0636fcdca9dbf6031de6c8aad62daf5
Brian McKenna <bmckenna1982@gmail.com>
2019-09-05 16:12:29 -0400

    final images embedded, nav completed, scroll sctions in js completed

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 function onLoad() {
-    setSectionBarWidth();           
+    setSectionBarWidth();
+    listen();           
 }
 
-function setSectionBarWidth () {
+function setSectionBarWidth() {
     let sectionClass = '';
     let h2Width = 0;
     $("section").each(function(index) {
@@ -13,5 +14,41 @@ function setSectionBarWidth () {
     })
 }
 
+function listen() {
+    $(".hero").on('click', '.page-link', goToSection );
+    $(".hero").on('click', '.anchor-link', goToSection );
+    $(window).on('scroll', activateNav );
+}
+
+function goToSection() {
+    let anchorID = $(this).attr("dest");
+    $('html, body').animate({
+        scrollTop: $(anchorID).offset().top - 50
+      }, 400);    
+    // $('.nav-container').find("*").removeClass('active');
+    highlightLink(anchorID);
+}
+
+function activateNav() {
+    let navPos = $('nav').position().top;
+    let pos = $(window).scrollTop();
+    let pos2 = pos + 50;
+    let scrollBottom = pos + $(window).height();
+    console.log(pos);
+    console.log($(document).height());
+
+    if (pos2 > $('#home-anchor').offset().top) {highlightLink('#home-anchor'); }
+    if (pos2 > $('#about-anchor').offset().top)      { highlightLink('#about-anchor'); }
+    if (pos2 > $('#projects-anchor').offset().top)      { highlightLink('#projects-anchor'); }
+    if (pos2 > $('#contact-anchor').offset().top ||
+        pos + $(window).height() === $(document).height()) {
+          highlightLink('#contact-anchor');
+    }
+}
+
+function highlightLink(anchor) {
+    $('nav .active').removeClass('active');
+    $("nav").find(`[dest="${anchor}"]`).addClass('active');
+  }
 
 $(onLoad)
